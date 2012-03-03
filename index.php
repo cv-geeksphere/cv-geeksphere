36ff0f25c585b28d7f74674821d65be4d5e7a5f3
laurence <laurenceputra@gmail.com>
2012-03-03 23:43:11 +0800

    initial stuff added

---
+++ index.php
@@ -0,0 +1,224 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Curriculum Vitae</title>
+        <meta name="description" content="">
+        <meta name="author" content="laurence, laurenceputra">
+        <!-- Le HTML5 shim, for IE6-8 support of HTML elements -->   
+        <!--[if lt IE 9]>
+          <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+        <![endif]-->
+        <!-- Le styles -->
+
+        <link href="css/bootstrap.css" rel="stylesheet" >
+        <link href="css/style.css" rel="stylesheet">
+        <link href="css/print.css" rel="stylesheet" media="print">
+        <link rel="shortcut icon" href="images/favicon.ico" >
+        <!--[if IE 6]><script type="text/javascript" src="http://deathtoie6.com/ie6-not-supported.js" ></script><![endif]-->
+    </head>
+    <body>
+        <div class="container">
+            <div class="span8">
+
+                <div class="thumbnail span3">
+                    <img src="images/me.jpg" alt="" />
+                </div>
+                <div class="hero-unit span4">
+                    <h1>Laurence Putra Franslay</h1>
+                    <h2>Geek;Hacker;Entrepreneur;</h2>
+                    <p>
+                        <i class="icon-map-marker"></i>
+                        Tembusu College;
+                        University Town, NUS;
+                        26 College Avenue East, #21-156;
+                        Singapore 138597;
+                    </p>
+                </div>
+                <div class="page-header">
+                </div>
+
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc mauris arcu, ultricies in vestibulum in, iaculis nec enim. Quisque sapien odio, venenatis mollis mollis id, pulvinar congue sapien. 
+                </p>
+                <p>
+
+                    Donec laoreet faucibus odio quis sollicitudin. Sed sagittis semper ultrices. Morbi eget eros felis, vel convallis nibh. Quisque ut felis magna, at lobortis sem. 
+                </p>
+                <div  class="page-header noPrint">
+                    <h2>Last work</h2>
+                </div>
+                <div id="myCarousel" class="carousel noPrint">
+                    <!-- Carousel items -->
+                    <div class="carousel-inner">
+                        <div class="active item">
+
+                            <img src="images/1.jpg" alt="" />
+                            <div class="carousel-caption">
+                                <h4>1 - Thumbnail label</h4>
+                                <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+                            </div>
+                        </div>
+                        <div class="item">
+                            <img src="images/2.jpg" alt="" />
+
+                            <div class="carousel-caption">
+                                <h4>2 - Thumbnail label</h4>
+                                <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+                            </div>
+                        </div>
+                        <div class="item">
+                            <img src="images/3.jpg" alt="" />
+                            <div class="carousel-caption">
+
+                                <h4>3 - Thumbnail label</h4>
+                                <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+                            </div>
+                        </div>
+                        <div class="item">
+                            <img src="images/4.jpg" alt="" />
+                            <div class="carousel-caption">
+                                <h4>4 - Thumbnail label</h4>
+
+                                <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- Carousel nav -->
+                    <a class="carousel-control left" href="#myCarousel" data-slide="prev">&lsaquo;</a>
+                    <a class="carousel-control right" href="#myCarousel" data-slide="next">&rsaquo;</a>
+                </div>
+
+                <div class="page-header">
+                    <h2>Work History</h2>
+                </div> 
+                <dl>
+
+                    <dt><i class="icon-asterisk"></i> Chalkboard Pte Ltd(<a href="http://yourchalkboard.com/" target="_blank">Link</a>)</dt>
+                    <ul>
+                        <li>Engineer(Intern): <strong>May 2011 - Aug 2011</strong></li>
+                        <li>Engineer(Contracted Consultant): <strong>Aug 2011 - Present</strong></li>
+                        <li>Joined Chalkboard as an intern as a fellow in the <a href="http://sg.startuproot.sg" target="_blank">StartupRootsSG</a> fellowship</li>
+                        <li>Programmed and Scaled Infrastructure and API</li>
+                        <li>Created Chalkboard <a href="http://widget.yourchalkboard.com/" target="_blank">Webwidgets</a> and <a href="http://publisher.yourchalkboard.com/" target="_blank">Publishers' Platform</a></li>
+                        <li>Conversion from SVN to Git</li>
+                    </ul>   
+
+                    <dt><i class="icon-asterisk"></i> National University of Singapore(<a href="http://nus.edu.sg/" target="_blank">Link</a>)</dt>
+                    <ul>
+                        <li>Undergraduate Discussion Group Leader(<a href="http://ivle7.nus.edu.sg/lms/Account/NUSBulletin/msearch_view.aspx?modeCode=CS1101S&acadYear=2010/2011&semester=1" target="_blank">CS1101S</a>): <strong>Aug 2010 - Nov 2010</strong></li>
+                        <li>Undergraduate Discussion Group Leader(<a href="http://ivle7.nus.edu.sg/lms/Account/NUSBulletin/msearch_view.aspx?modeCode=CS1010&acadYear=2011/2012&semester=1" target="_blank">CS1010</a>): <strong>Aug 2011 - Nov 2011</strong></li>
+                        <li>Taught programming classes in Scheme for CS1101S, and C for CS1010</li>
+                    </ul>
+
+                    <dt><i class="icon-asterisk"></i>Hulutech(<a href="http://www.hulutech.com/" target="_blank">Link</a>)</dt>
+                    <ul>
+                        <li>Engineer: <strong>Jan 2011 - April 2011</strong></li>
+                        <li>On the team redesigning the 65Daigou site, a project which was eventually scrapped</li>
+                        <li>Created a backend book ordering system for <a href="http://sg.mpmmath.com/" target="_blank">MPMmath</a></li>
+                    </ul>
+
+                    <dt><i class="icon-asterisk"></i>Computing for Voluntary Welfare Organisations(<a href="http://www.comp.nus.edu.sg/~vwo/" target="_blank">Link</a>)</dt>
+                    <ul>
+                        <li>Engineer: <strong>May 2010 - May 2011</strong></li>
+                        <li>Implemented the MINDS eClient system</li>
+                        <li>Maintained the server, with specific focus on security</li>
+                    </ul>
+                </dl>  
+
+                <div class="page-header">
+                    <h2>Side Projects</h2>
+                </div> 
+                <dl>
+                    <dt><i class="icon-asterisk"></i> StartupRootsSG(<a href="http://sg.startuproots.org/" target="_blank">Link</a>)</dt>
+                    <ul>
+                        <li>Organiser: <strong>Jan 2012 - Present</strong></li>
+                        <li>Linked the top startups in Singapore to the top students</li>
+                        <li>Interviewed (technical) students</li>
+                        <li>Planned and organised events for the duration of the fellowship</li>
+                    </ul>
+                    <dt><i class="icon-asterisk"></i> IWasntPaid.com(<a href="http://iwasntpaid.com" target="_blank">Link</a>)</dt>
+                    <ul>
+                        <li>Founder: <strong>Jan 2012 - Present</strong></li>
+                        <li>Website for freelance developers to report their horror stories and hopefully get paid for what they have done</li>
+                        <li>Helped a few freelancers get their pay back</li>
+                    </ul>
+                    <dt><i class="icon-asterisk"></i> GeekcampSG(<a href="http://geekcamp.pbworks.com" target="_blank">Link</a>)</dt>
+                    <ul>
+                        <li>Organiser: <strong>July 2011 - Present</strong></li>
+                        <li>Organised GeekcampSG, one of the largest Geek conferences in Singapore</li>
+                        <li>On the organising team with 6 other awesome geeks.</li>
+                    </ul>
+                </dl>  
+
+                <div class="page-header">
+                    <h2>Technology I play with</h2>
+                </div> 
+                <dl>
+                    <dd>C/C++, C#/.NetFramework, PHP/Codeigniter, Python/Django, nginx, Apache, CentOS/Fedora, mongoDB, PostgreSQL/PostGIS, memcached, LATEX, HTML5, CSS3, Javascript</dd>
+                </dl> 
+
+                <div class="page-header">
+                    <h2>Education</h2>
+                </div>
+                <dl>
+                    <dt><i class="icon-asterisk"></i> National University of Singapore(<a href="http://nus.edu.sg/" target="_blank">Link</a>)</dt>
+                    <ul>
+                        <li>Bachelor of Computing in Computer Science: <strong>Aug 2009 - Present</strong></li>
+                        <li>NUSHackers(<a href="http://nushackers.org" target="_blank">Link</a>): <strong>Aug 2010 - Present</strong>
+                            <ul>
+                                <li>Coreteam member</li>
+                                <li>Organised Hack Workshop Series, which was supported by the iDA</li>
+                                <li>Organised Friday Hacks, a weekly event to get students to build things</li>
+                                <li>Organised Hack&Roll, a 24 hour Hackathon</li>
+                            </ul>
+                        </li>
+                        <li>Computing for Voluntary Welfare Organisations(<a href="http://www.comp.nus.edu.sg/~vwo/" target="_blank">Link</a>): <strong>Aug 2009 - May 2011</strong>
+                            <ul>
+                                <li>Held workshops teaching the new applicants about PHP, as well as the LAMP stack</li>
+                                <li>Created and graded the PHP assignments</li>
+                            </ul>
+                        </li>
+                    </ul>
+                </dl>  
+
+            </div>
+
+            <div class="span3">
+                <h2>Contact</h2>
+                <p><i class="icon-envelope"></i> <script type="text/javascript">
+<!--
+    var string1 = "laurenceputra";var string2 = "@";var string3 = "gmail.com";var string4 = string1 + string2 + string3;document.write("<a href=" + "mail" + "to:" + string1 + string2 + string3 + ">" + string4 + "</a>");
+//-->
+</script>
+                </p>
+                <a href="#" class="btn btn-large noPrint" id="printBtn"><i class="icon-print"></i> Print</a>
+
+                <a href="Curriculum_Vitae.pdf" class="btn btn-large noPrint" id="downBtn"><i class="icon-download"></i> Download</a>
+            </div>
+        </div>
+
+
+        <div class="container">
+            <footer>
+                <p>
+                    <i class="icon-envelope"></i> 
+<script type="text/javascript">
+<!--
+    var string1 = "laurenceputra";var string2 = "@";var string3 = "gmail.com";var string4 = string1 + string2 + string3;document.write("<a href=" + "mail" + "to:" + string1 + string2 + string3 + ">" + string4 + "</a>");
+//-->
+</script> &nbsp; 
+                    <i class="icon-map-marker"></i>
+                    Tembusu College;
+                    University Town, NUS;
+                    26 College Avenue East, #21-156;
+                    Singapore 138597;
+                </p>
+            </footer>
+        </div>
+        <script src="js/jquery.js"></script>
+        <script src="js/bootstrap.js"></script>
+        <script src="js/scripts.js"></script>
+    </body>
+
+</html>
